e53c56a4e46d905d80284af26b33a8c102148a8a
Luis-caguiat <90353580+Luis-caguiat@users.noreply.github.com>
2021-11-19 14:12:12 -0800

    Add files via upload

--- Assignment4.py
+++ Assignment4.py
@@ -9,16 +9,15 @@ class Node:
         self.children = []
         self.node_name = ''.join([random.choice(string.ascii_letters) for i in range(3)])
 
-   def make_children(self, current_layer_number, node_per_layer_map):
-        #when to terminate the recursion
+        
+   def make_children(self, current_layer_number, node_per_layer_map):   
         if current_layer_number >= len(node_per_layer_map):
-            return
+            return       
         
         for i in range(node_per_layer_map[current_layer_number]):
-            self.children.append( Node())
-        
+            self.children.append( Node())        
         self.children[0].make_children( current_layer_number + 1, node_per_layer_map)
-         
+        
         for i in range (1, len(self.children)) :
             self.children[i].children = self.children[0].children[:]
             
